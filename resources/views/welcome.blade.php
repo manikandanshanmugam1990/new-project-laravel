ad47fd3a838e7cfb99d340f5beaf03bdcc7e94cb
manikandan <msmani94@gmail.com>
2023-11-14 19:56:16 +0530

    New Upated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+    <head >
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -31,7 +31,7 @@
                 </div>
             @endif
 
-            <p>contact: 0000000000 6556464</p>
+            <p>contact: 0000000000 655646655444</p>
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8 col-xs-12">
                 <div class="flex justify-center">
