87d546c3465113d31fc24f817d25b538402b56dd
manikandan <msmani94@gmail.com>
2023-11-27 11:20:50 +0530

    chagnes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,7 +133,7 @@
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                        Laravel 123 465 v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel illuminate 123 465 v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
